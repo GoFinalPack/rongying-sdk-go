ed9a6fd1af3271b2bf171485e95fa9144ba16128
PFinal_南丞 <1370288056@qq.com>
2023-08-31 14:00:00 +0800

    2023-08-31 脱敏

--- test/util_test.go
+++ test/util_test.go
@@ -22,5 +22,5 @@ func TestSendPost(t *testing.T) {
 	params := map[string]string{
 		"Mobile": "17621166911",
 	}
-	fmt.Println(rongying.SendPost("https://wdapi.yuntongxin.vip/query/seat/detail/v3?Sig=5AE42666B0BB03A982DBC06D41175135", params, "Njc2NzYxOGQzYzc1NGI1M2E3MTk1NjQ5YTdiYTAzM2Q4MDRjYjYzNWMwMjE0ODc2YWIxODJlOWJhM2FhOTgxYjIwMjMwODMwMTYwNDM4"))
+	fmt.Println(rongying.SendPost("https://wdapi.yuntongxin.vip/query/seat/detail/v3?Sig=xxxxxxx", params, "xxxxxxxxx"))
 }
